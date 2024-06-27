0f419f0d34c4388d192d721381281373515b9b58
various-authors <various.authors@gmail.com>
2024-06-26 21:12:08 -0700

    fix: update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 mkdir -p build objects
 
-rgbgfx -T -h -o assets/valley-graphics-8x8-tiles.2bpp assets/valley-graphics-8x8-tiles.png
-rgbgfx -T -h -o assets/valley-map-8x8-tiles.2bpp assets/valley-map-8x8-tiles.png
-rgbgfx -T -h -o assets/valley-sprites-8x8-tiles.2bpp assets/valley-sprites-8x8-tiles.png
-rgbgfx -T -h -o assets/valley-additional-8x8-tiles.2bpp assets/valley-additional-8x8-tiles.png
+./bin/rgbgfx -T -h -o assets/valley-graphics-8x8-tiles.2bpp assets/valley-graphics-8x8-tiles.png
+./bin/rgbgfx -T -h -o assets/valley-map-8x8-tiles.2bpp assets/valley-map-8x8-tiles.png
+./bin/rgbgfx -T -h -o assets/valley-sprites-8x8-tiles.2bpp assets/valley-sprites-8x8-tiles.png
+./bin/rgbgfx -T -h -o assets/valley-additional-8x8-tiles.2bpp assets/valley-additional-8x8-tiles.png
 
-rgbasm -i src -o objects/main.o src/main.asm
-rgblink -n build/showdown.sym -m build/showdown.map -o build/showdown.gb objects/main.o
-rgbfix -v -p 0 build/showdown.gb
+./bin/rgbasm -i src -o objects/main.o src/main.asm
+./bin/rgblink -n build/showdown.sym -m build/showdown.map -o build/showdown.gb objects/main.o
+./bin/rgbfix -v -p 0 build/showdown.gb
