7afe6b6d69cc0ad41bba9e22d37f19a6303e5187
various-authors <various.authors@gmail.com>
2024-09-13 14:07:53 -0700

    feat: initial dialog

--- src/scripts/text-encode.js
+++ src/scripts/text-encode.js
@@ -1,33 +1,41 @@
-const text = process.argv[2]
+const [a, b, ...texts] = process.argv;
 
-if (text.length > 17) {
-  console.log('TEXT TOO LONG')
-  console.log('.................|')
-  console.log(`${text.slice(0, 17)}|${text.slice(17)}`)
+console.log(a, b, texts)
 
-  return
-} else {
-  console.log('.................|')
-  console.log(`${text.padEnd(17, '.')}|`)
+texts.forEach((text) => {
+  if (text.length > 17) {
+    console.log("TEXT TOO LONG");
+    console.log(".................|");
+    console.log(`${text.slice(0, 17)}|${text.slice(17)}`);
 
-}
+    return;
+  } else {
+    console.log(".................|");
+    console.log(`${text.padEnd(17, ".")}|`);
+  }
+})
 
-console.log(
-  "db",
-  text
-    .split("")
-    .map((ch) => ch.charCodeAt(0))
-    .map((ch) => ch - 62)
-    .map((ch) => {
-      switch (ch) {
-        case -30: // space
-          return 2;
-        case -16: // period
-          return 29;
-        default:
-          return ch;
-      }
-    })
-    .join(", "),
-  ", LINE_FEED"
-);
+console.log(`; node ./src/scripts/text-encode.js`, texts.map((text) => `"${text}"`).join(' '))
+
+texts.forEach((text, i) => {
+  console.log(
+    "db",
+    text
+      .toUpperCase()
+      .split("")
+      .map((ch) => ch.charCodeAt(0))
+      .map((ch) => ch - 62)
+      .map((ch) => {
+        switch (ch) {
+          case -30: // space
+            return 2;
+          case -16: // period
+            return 29;
+          default:
+            return ch;
+        }
+      })
+      .join(", "),
+    ", LINE_FEED", i === texts.length - 1 ? ", NULL" : ""
+  );
+});
